ab01bf23340950ab9a5af5288ca0c45528586ae8
Karamjeet Singh <karamjeet.jandu@regalix-inc.com>
2019-09-24 12:06:31 +0530

    profile template showing data

--- employee-ps/employee/views/profile.py
+++ employee-ps/employee/views/profile.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
+from employee.models import Employee
 
 profile_bp = Blueprint('profile_bp', __name__)
 
 @profile_bp.route('/profile')
 def profile():
-	return render_template('profile.html')
+	emp = Employee.query.filter_by(id=session['id']).first()
+	return render_template('profile.html', emp=emp)
